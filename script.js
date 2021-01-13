b6793acb6984e64b0eb93894e99d4f8a3f2449ba
Stephanie Taylor <sjean.taylor5@gmail.com>
2021-01-13 03:07:33 -0500

    fixed all BEM files

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let popup = document.getElementById('editProfile');
-let editButton = document.querySelector('.profile__info_edit-btn');
-let submitButton = document.querySelector('.popup__container_submit');
-let closeButton = document.querySelector('.popup__container_close-btn');
-let nameInput = document.querySelector('.popup__container_field-one');
-let aboutInput = document.querySelector('.popup__container_field-two');
-let headerName = document.querySelector('.profile__info_name-title');
-let aboutMe = document.querySelector('.profile__info_explorer');
+let editButton = document.querySelector('.profile__edit-btn');
+let submitButton = document.querySelector('.popup__submit');
+let closeButton = document.querySelector('.popup__close-btn');
+let nameInput = document.querySelector('.popup__name-input');
+let aboutInput = document.querySelector('.popup__about-input');
+let headerName = document.querySelector('.profile__title');
+let aboutMe = document.querySelector('.profile__explorer');
 let pictureLike = document.querySelector('.pictures');
 
 editButton.addEventListener('click', openPopup);
