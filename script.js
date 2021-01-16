c8b4878dbc6c57479243d4ead59581e54cc23054
Stephanie Taylor <sjean.taylor5@gmail.com>
2021-01-15 21:25:48 -0500

    fixed all js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let popup = document.getElementById('editProfile');
+let popup = document.querySelector('.popup');
 let editButton = document.querySelector('.profile__edit-btn');
 let submitButton = document.querySelector('.popup__submit');
 let closeButton = document.querySelector('.popup__close-btn');
@@ -6,13 +6,6 @@ let nameInput = document.querySelector('.popup__name-input');
 let aboutInput = document.querySelector('.popup__about-input');
 let headerName = document.querySelector('.profile__title');
 let aboutMe = document.querySelector('.profile__explorer');
-let pictureLike = document.querySelector('.pictures');
-
-editButton.addEventListener('click', openPopup);
-closeButton.addEventListener('click', closePopup);
-submitButton.addEventListener('click', submitInput);
-pictureLike.addEventListener('click', toggleLike);
-
 
 function openPopup() {
     popup.classList.add('popup_opened');
@@ -24,22 +17,11 @@ function closePopup() {
 
 function submitInput(event) {
     event.preventDefault();
-    let name = nameInput.value;
-    let about = aboutInput.value;
-    headerName.innerText = name;
-    aboutMe.innerText = about;
-    nameInput.value = '';
-    aboutInput.value = '';
+    headerName.innerText = nameInput.value;
+    aboutMe.innerText = aboutInput.value;
     closePopup();
 }
 
-function toggleLike(event) {
-    let element = event.target;
-    if (element.alt === "Heart") {
-        if (element.src.includes("like-button.svg")) {
-            element.src = "./images/active-like.svg";
-        } else {
-            element.src = "./images/like-button.svg";
-        }
-    }
-}
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
+submitButton.addEventListener('click', submitInput);
