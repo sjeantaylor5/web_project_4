ced4713ef491db79717e168cbf89cd17ee6690e5
Stephanie Taylor <sjean.taylor5@gmail.com>
2021-01-21 02:25:46 -0500

    working on javescript for buttons on page

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,14 @@
-let popup = document.querySelector('.popup');
-let editForm = document.querySelector('.popup__container');
-let editButton = document.querySelector('.profile__edit-btn');
-let closeButton = document.querySelector('.popup__close-btn');
-let nameInput = document.querySelector('.popup__name-input');
-let aboutInput = document.querySelector('.popup__about-input');
-let headerName = document.querySelector('.profile__title');
-let aboutMe = document.querySelector('.profile__explorer');
+const popup = document.querySelector('.popup');
+const addpic = document.querySelector('.popup_type_addpic');
+const editForm = document.querySelector('.popup__container');
+const editButton = document.querySelector('.profile__edit-btn');
+const addButton = document.querySelector('.profile__add-btn');
+const closeAddButton = document.querySelector('.popup__close-btn_type_addpic');
+const closeButton = document.querySelector('.popup__close-btn');
+const nameInput = document.querySelector('.popup__name-input');
+const aboutInput = document.querySelector('.popup__about-input');
+const headerName = document.querySelector('.profile__title');
+const aboutMe = document.querySelector('.profile__explorer');
 
 function openPopup() {
     nameInput.value = headerName.innerText;
@@ -13,8 +16,16 @@ function openPopup() {
     popup.classList.add('popup_opened');
 }
 
+function openAddpic() {
+    addpic.classList.add('popup_opened');
+}
+
 function closePopup() {
     popup.classList.remove('popup_opened');
+}
+
+function closeAddpic() {
+    addpic.classList.remove('popup_opened');
 }
 
 function submitInput(event) {
@@ -27,3 +38,5 @@ function submitInput(event) {
 editButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
 editForm.addEventListener('submit', submitInput);
+addButton.addEventListener('click', openAddpic);
+closeAddButton.addEventListener('click', closeAddpic);
