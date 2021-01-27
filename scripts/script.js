98c05b9116a1c7ad1caedf3a2b87fa243f1c3bf7
Stephanie Taylor <sjean.taylor5@gmail.com>
2021-01-27 02:27:03 -0500

    Made all corrections to project

--- scripts/script.js
+++ scripts/script.js
@@ -1,135 +1,87 @@
-const popup = document.querySelector('.popup');
+const profilePopup = document.querySelector('.popup_type_profile');
 const imagePopup = document.querySelector('.popup_type_image');
 const addpic = document.querySelector('.popup_type_addpic');
 const editForm = document.querySelector('.popup__container');
+const addForm = document.querySelector('.popup__container_type_addpic');
 const editButton = document.querySelector('.profile__edit-btn');
 const addButton = document.querySelector('.profile__add-btn');
 const addCard = document.querySelector('.popup_type_addpic');
 const createPic = document.querySelector('.popup__submit_type_addpic');
 const closeAddButton = document.querySelector('.popup__close-btn_type_addpic');
 const closeButton = document.querySelector('.popup__close-btn');
-const closeImageButton = document.querySelector('.popup__close-btn_type_image');
+const popupImage = imagePopup.querySelector('.popup__image');
+const popupImageTitle = imagePopup.querySelector('.popup__image-title');
+const closeImageButton = imagePopup.querySelector('.popup__close-btn');
 const nameInput = document.querySelector('.popup__name-input');
 const aboutInput = document.querySelector('.popup__about-input');
 const titleInput = document.querySelector('.popup__name-input_type_addpic');
 const linkInput = document.querySelector('.popup__about-input_type_addpic');
 const headerName = document.querySelector('.profile__title');
 const aboutMe = document.querySelector('.profile__explorer');
-const picturesTemplate = document.querySelector('.pictures');
+const picturesTemplate = document.querySelector('.card-template').content.querySelector('.picture');
+const list = document.querySelector('.pictures__list');
 
-const initialCards = [{
-        src: "https://images.unsplash.com/photo-1463695970743-ae65cca05743?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
-        alt: "Waterfall",
-        text: "Niagara Falls"
-    },
-    {
-        src: "https://images.unsplash.com/photo-1525153770748-acfaa305783b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=632&q=80",
-        alt: "Lake",
-        text: "Lake Tahoe"
-    },
-    {
-        src: "https://images.unsplash.com/photo-1529963183134-61a90db47eaf?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
-        alt: "Northern Lights",
-        text: "Iceland"
-    },
-    {
-        src: "https://images.unsplash.com/photo-1591587793878-bd37e8bd73e9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=926&q=80",
-        alt: "Washington Monument",
-        text: "Washington Monument"
-    },
-    {
-        src: "https://images.unsplash.com/photo-1587162146766-e06b1189b907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1125&q=80",
-        alt: "City Skyline",
-        text: "New York City"
-    },
-    {
-        src: "https://images.unsplash.com/photo-1536597297293-f5adf6145863?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
-        alt: "The Harbor",
-        text: "Pearl Harbor"
-    }
-];
+function togglePopupWindow(modal) {
+    modal.classList.toggle('popup_opened');
+}
 
-const cardElements = initialCards.map(card => {
-    return createCard(card.src, card.alt, card.text);
-});
+initialCards.forEach(data => {
+    const cardElement = picturesTemplate.cloneNode(true);
 
-picturesTemplate.append(...cardElements);
+    const trashButton = cardElement.querySelector('.pictures__trash');
+    const linkImage = cardElement.querySelector('.pictures__image');
+    const titleName = cardElement.querySelector('.pictures__title');
+    const likeButton = cardElement.querySelector('.pictures__like');
 
-
-function createCard(src, alt, text) {
-    const cardBlock = document.createElement('div');
-    const trashButton = document.createElement('button');
-    const linkImage = document.createElement('img');
-    const titleName = document.createElement('h2');
-    const likeButton = document.createElement('button');
-
-    cardBlock.classList.add('pictures__picture');
-    trashButton.classList.add('pictures__trash');
-    linkImage.classList.add('pictures__image');
-    titleName.classList.add('pictures__title');
-    likeButton.classList.add('pictures__like');
-
-    linkImage.setAttribute('src', src);
-    linkImage.setAttribute('alt', alt);
-    titleName.innerText = text;
-
+    linkImage.src = data.src;
+    titleName.textContent = data.text;
     trashButton.addEventListener('click', deleteCard);
     likeButton.addEventListener('click', toggleButton);
     linkImage.addEventListener('click', () => {
-        const popupImage = document.querySelector('.popup__image');
-        const popupImageTitle = document.querySelector('.popup__image-title');
+        popupImage.src = data.src;
+        popupImageTitle.textContent = data.text;
 
-        popupImage.src = src;
-        popupImageTitle.textContent = text;
+        togglePopupWindow(imagePopup);
+    })
 
-        openImage();
-    });
-
-    cardBlock.append(...[trashButton, linkImage, titleName, likeButton]);
-    return cardBlock;
-}
-
-
-function openPopup() {
-    nameInput.value = headerName.innerText;
-    aboutInput.value = aboutMe.innerText;
-    popup.classList.add('popup_opened');
-}
-
-function openAddpic() {
-    addpic.classList.add('popup_opened');
-}
-
-function openImage() {
-    imagePopup.classList.add('popup_opened');
-}
-
-function closePopup() {
-    popup.classList.remove('popup_opened');
-}
-
-function closeAddpic() {
-    addpic.classList.remove('popup_opened');
-}
-
-function closeImage() {
-    imagePopup.classList.remove('popup_opened');
-}
+    list.prepend(cardElement);
+});
 
 function addNewCard(event) {
     event.preventDefault();
-    const title = titleInput.value;
-    const link = linkInput.value;
-    const cardBlock = createCard(link, title, title);
-    picturesTemplate.prepend(cardBlock);
+
+    const cardElement = picturesTemplate.cloneNode(true);
+
+    const trashButton = cardElement.querySelector('.pictures__trash');
+    const linkImage = cardElement.querySelector('.pictures__image');
+    const titleName = cardElement.querySelector('.pictures__title');
+    const likeButton = cardElement.querySelector('.pictures__like');
+
+    linkImage.src = linkInput.value;
+    titleName.textContent = titleInput.value;
+
+    trashButton.addEventListener('click', deleteCard);
+    likeButton.addEventListener('click', toggleButton);
+
+    linkImage.addEventListener('click', () => {
+        popupImage.src = linkImage.src;
+        popupImageTitle.textContent = titleName.textContent;
+
+        togglePopupWindow(imagePopup);
+    })
+
+    list.prepend(cardElement);
+
     titleInput.value = "";
     linkInput.value = "";
-    closeAddpic();
+
+    togglePopupWindow(addCard);
 }
+
 
 function deleteCard(event) {
     const deleted = event.target.parentElement;
-    picturesTemplate.removeChild(deleted);
+    list.removeChild(deleted);
 }
 
 function toggleButton(event) {
@@ -137,17 +89,38 @@ function toggleButton(event) {
     likeButton.classList.toggle('pictures__like_active');
 }
 
+function openProfilePopup() {
+    nameInput.value = headerName.innerText;
+    aboutInput.value = aboutMe.innerText;
+
+    togglePopupWindow(profilePopup);
+}
+
 function submitInput(event) {
     event.preventDefault();
     headerName.innerText = nameInput.value;
     aboutMe.innerText = aboutInput.value;
-    closePopup();
+
+    togglePopupWindow(profilePopup);
 }
 
-editButton.addEventListener('click', openPopup);
-closeButton.addEventListener('click', closePopup);
 editForm.addEventListener('submit', submitInput);
-addButton.addEventListener('click', openAddpic);
-createPic.addEventListener('click', addNewCard);
-closeAddButton.addEventListener('click', closeAddpic);
-closeImageButton.addEventListener('click', closeImage);
+addForm.addEventListener('submit', addNewCard);
+editButton.addEventListener('click', openProfilePopup);
+
+
+closeButton.addEventListener('click', () => {
+    togglePopupWindow(profilePopup);
+});
+
+addButton.addEventListener('click', () => {
+    togglePopupWindow(addCard);
+});
+
+closeAddButton.addEventListener('click', () => {
+    togglePopupWindow(addCard);
+});
+
+closeImageButton.addEventListener('click', () => {
+    togglePopupWindow(imagePopup);
+});
